77ab40334a95197ab4592b614cabb007db0fdd44
Stian Westvig <stian.westvig@bouvet.no>
2014-10-25 21:34:38 +0200

    added video stream

--- js/controllers/tabs-controller.js
+++ js/controllers/tabs-controller.js
@@ -6,17 +6,17 @@ app.controller('tabsController', function(){
         user: 'Stian',
         longitude: 10.594261,
         latitude: 59.436796,
-        content: 'hei Stian'
+        stream: 'http://192.168.1.228'
     }, {
         user: 'Rittik',
         longitude: 10.554261,
         latitude: 59.435796,
-        content: 'hei Rittik'
+        stream: 'http://192.168.1.251'
     }, {
         user: 'Tor Erling',
         longitude: 10.534261,
         latitude: 59.434796,
-        content: 'hei Tor Erling'
+        stream: 'http://192.168.1.251'
     }];
 
 });
