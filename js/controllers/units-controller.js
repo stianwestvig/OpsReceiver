ba99a58f1b2dba311fe5e39c6f971502812c7561
Stian Westvig <stian.westvig@bouvet.no>
2014-10-25 23:13:48 +0200

    work on socket and maps integration

--- js/controllers/units-controller.js
+++ js/controllers/units-controller.js
@@ -2,21 +2,89 @@
 app.controller('unitsController', function(){
     var units = this;
 
-    units.data = [{
-        user: 'Stian',
-        longitude: 10.594261,
-        latitude: 59.436796,
-        stream: 'http://192.168.1.228'
-    }, {
-        user: 'Rittik',
-        longitude: 10.554261,
-        latitude: 59.435796,
-        stream: 'http://192.168.1.251'
-    }, {
-        user: 'Tor Erling',
-        longitude: 10.534261,
-        latitude: 59.434796,
-        stream: 'http://192.168.1.251'
-    }];
+    units.data = [];
+
+    window.datahubSocket = new WebSocket("ws://nanopils.servebeer.com:2233/ws", "protocolOne");
+
+    datahubSocket.onopen = function (event) {
+        datahubSocket.send("OpsCast socket connected");
+    };
+
+    datahubSocket.onmessage = function (event) {
+        console.log(event);
+
+
+
+        // users:
+        if (event.data.topic === 'setUser') {
+            console.log('units.data', units.data);
+            console.log('event.data.payload', event.data.payload);
+
+            if(! $.inArray(event.data.payload.user.id, units.data)) {
+                units.data.push(event.data.payload.user)
+            }
+        }
+
+
+
+        // locations:
+        if (event.data.topic === 'locationUpdate') {
+            var posInArray = $.inArray(event.data.payload.user.id, units.data);
+            if (posInArray > 0) {
+                var myLatlng = new google.maps.LatLng(
+                    event.data.payload.location.latitude,
+                    event.data.payload.location.longitude
+                );
+                var mapOptions = {
+                    zoom: 4,
+                    center: myLatlng
+                };
+                var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+
+                var marker = new google.maps.Marker({
+                    position: myLatlng,
+                    title: event.data.payload.user.name
+                });
+
+                marker.setMap(map);
+
+                units.data[posInArray].marker = marker;
+            }
+
+            console.log('units.data after locationUpdate', units.data);
+        }
+
+        messageBus.broadcast(event.data + '; Receiver triggered by datahub was here');
+
+    };
+
+
+
+    var location = {
+        topic: 'locationUpdate',
+        payload: {
+            location: {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                accuracy: position.coords.accuracy
+            }
+        },
+        user: {}
+    };
+
+
+    var example = {
+        topic: 'setUser',
+        payload: {
+            user: {
+                name: username,
+                ip: ip,
+                source: 'app',
+                id: 'some socket id'
+            }
+        }
+    };
+
+
 
 });
