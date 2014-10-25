13f6ad14d3cd940b2a99f19042c900866520e431
Nils Perret <nils.perret@gmail.com>
2014-10-25 15:21:29 +0200

    Simple websocket communication against datahub that updates text

--- js/datahubCommunication.js
+++ js/datahubCommunication.js
@@ -1,4 +1,13 @@
 function setupDataHubCommunication() {
-    // var exampleSocket = new WebSocket("ws://nanopils.servebeer.com:2233/ws", "protocolOne");
+	var datahubSocket = new WebSocket("ws://nanopils.servebeer.com:2233/ws", "protocolOne");
+
+	datahubSocket.onopen = function (event) {
+		datahubSocket.send("OpsCast socket connected"); 
+	};
+
+	datahubSocket.onmessage = function (event) {
+		console.log(event);
+		displayText(event.data);
+	}
 }
 
