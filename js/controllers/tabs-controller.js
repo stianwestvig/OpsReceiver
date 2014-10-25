70c8593d628a656abb2d56aee397ed9d876c1d86
Stian Westvig <stian.westvig@bouvet.no>
2014-10-25 21:10:36 +0200

    added angular logic

--- js/controllers/tabs-controller.js
+++ js/controllers/tabs-controller.js
@@ -2,18 +2,21 @@
 app.controller('tabsController', function(){
     var tabs = this;
 
-    tabs.tabs = [{
+    tabs.data = [{
         user: 'Stian',
         longitude: 10.594261,
-        latitude: 59.436796
+        latitude: 59.436796,
+        content: 'hei Stian'
     }, {
         user: 'Rittik',
         longitude: 10.554261,
-        latitude: 59.435796
+        latitude: 59.435796,
+        content: 'hei Rittik'
     }, {
         user: 'Tor Erling',
         longitude: 10.534261,
-        latitude: 59.434796
+        latitude: 59.434796,
+        content: 'hei Tor Erling'
     }];
 
 });
