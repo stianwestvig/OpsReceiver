018c369aa71f09d6ef632921e87f3ef9acf43bc2
Stian Westvig <stian.westvig@bouvet.no>
2014-10-25 21:05:44 +0200

    added angular

---
+++ js/controllers/tabs-controller.js
@@ -0,0 +1,19 @@
+
+app.controller('tabsController', function(){
+    var tabs = this;
+
+    tabs.tabs = [{
+        user: 'Stian',
+        longitude: 10.594261,
+        latitude: 59.436796
+    }, {
+        user: 'Rittik',
+        longitude: 10.554261,
+        latitude: 59.435796
+    }, {
+        user: 'Tor Erling',
+        longitude: 10.534261,
+        latitude: 59.434796
+    }];
+
+});
