2d250e8530c61ff9c9d53880b294829dcf50e4dc
Nils Perret <nils.perret@gmail.com>
2014-10-26 08:39:45 +0100

    Trying to fix communication bug

--- js/datahubCommunication.js
+++ js/datahubCommunication.js
@@ -1,16 +1,16 @@
 function setupDataHubCommunication() {
-	window.datahubSocket = new WebSocket("ws://nanopils.servebeer.com:2233/ws", "protocolOne");
+	// window.datahubSocket = new WebSocket("ws://nanopils.servebeer.com:2233/ws", "protocolOne");
 
-	datahubSocket.onopen = function (event) {
-		datahubSocket.send("OpsCast socket connected"); 
-	};
+	// datahubSocket.onopen = function (event) {
+	// 	datahubSocket.send("OpsCast socket connected"); 
+	// };
 
-	datahubSocket.onmessage = function (event) {
-		console.log(event);
-		displayText(event.data);
-		// if (event.data.type === 'loggedIn') {
-			messageBus.broadcast(event.data);
-		// }
-	}
+	// datahubSocket.onmessage = function (event) {
+	// 	console.log(event);
+	// 	displayText(event.data);
+	// 	// if (event.data.type === 'loggedIn') {
+	// 		messageBus.broadcast(event.data);
+	// 	// }
+	// }
 }
 
